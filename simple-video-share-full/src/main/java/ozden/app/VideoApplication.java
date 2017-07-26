99686fc731d01829cc97025b3a62a808c9be2902
tahsinozden <tahsinozden@gmail.com>
2017-07-26 22:46:32 +0200

    name refactoring.

---
+++ simple-video-share-full/src/main/java/ozden/app/VideoApplication.java
@@ -0,0 +1,18 @@
+package ozden.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
+@EnableScheduling
+public class VideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoApplication.class, args);
+	}
+}
