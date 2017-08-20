0de27e280b41a68155f4e368836c473fac4742f6
tahsinozden <tahsinozden@gmail.com>
2017-08-20 22:46:15 +0200

    add random video tag display feature.

--- simple-video-share-full/src/main/java/ozden/app/video/persistance/VideoRepository.java
+++ simple-video-share-full/src/main/java/ozden/app/video/persistance/VideoRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface VideoRepository extends JpaRepository<Video, Integer> {
     List<Video> findByVideoId(Integer videoId);
     List<Video> findByVideoName(String videoName);
+    List<Video> findByVideoFilePathLike(String pathLike);
 }
