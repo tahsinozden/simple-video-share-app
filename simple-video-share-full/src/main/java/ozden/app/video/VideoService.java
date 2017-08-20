37de18845c4869a6d0a17ca01364602ba87a72ef
tahsinozden <tahsinozden@gmail.com>
2017-08-20 11:10:24 +0200

    code formatting.

--- simple-video-share-full/src/main/java/ozden/app/video/VideoService.java
+++ simple-video-share-full/src/main/java/ozden/app/video/VideoService.java
@@ -11,23 +11,23 @@ import java.util.Optional;
 @Service
 public class VideoService {
 
-	@Autowired
-	private FileService fileService;
+    @Autowired
+    private FileService fileService;
 
-	public Optional<String> getRandomVideoName(String videoFilesPath) {
-		Optional<File> file = fileService.getRandomFileWithExtension(videoFilesPath, SupportedVideoFormat.MP4.getName());
-		return file.isPresent() ? Optional.of(file.get().getName()) : Optional.empty();
-	}
+    public Optional<String> getRandomVideoName(String videoFilesPath) {
+        Optional<File> file = fileService.getRandomFileWithExtension(videoFilesPath, SupportedVideoFormat.MP4.getName());
+        return file.isPresent() ? Optional.of(file.get().getName()) : Optional.empty();
+    }
 
-	public InputStream getVideoStream(String videoPath) {
-		InputStream is = null;
-		try {
-			is = fileService.getFileStream(videoPath);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    public InputStream getVideoStream(String videoPath) {
+        InputStream is = null;
+        try {
+            is = fileService.getFileStream(videoPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		return is;
-	}
+        return is;
+    }
 }
 
